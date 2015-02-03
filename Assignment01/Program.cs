bed5741f59aea11a7ba2a976c15e54d334d3d4f9
aramrattan <arlina.ramrattan@gmail.com>
2015-02-03 12:49:43 -0500

    Testing to see if fight method works

--- Assignment01/Program.cs
+++ Assignment01/Program.cs
@@ -10,8 +10,16 @@ namespace Assignment01
     {
         static void Main(string[] args)
         {
+            string consoleKey = "";
             Hero Xena = new Hero("Xena");
             Xena.show();
+
+            //used to check if all features are working as should
+            while (consoleKey != "q")
+            {
+                Xena.fight();
+                consoleKey = Console.ReadLine();
+            }
         }
     }
 }
