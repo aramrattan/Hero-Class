b14fad8a0cb98252ad1cf9d977e90800c588452f
aramrattan <arlina.ramrattan@gmail.com>
2015-02-03 12:42:38 -0500

    Added Private & Public properties and Constructor method

--- Assignment01/Hero.cs
+++ Assignment01/Hero.cs
@@ -8,5 +8,19 @@ namespace Assignment01
 {
     class Hero
     {
+        //******************************************Private Properties*********************************************************
+        private int strength;
+        private int speed;
+        private int health;
+
+        //*****************************************Public Properties***********************************************************
+        public string name;
+
+        //*****************************************Constructor Method**********************************************************
+        public Hero(string name)
+        {
+            this.name = name;
+            generateAbilities();
+        }
     }
 }
